5aee4548ce2df38ce43e46f4ac1d42c419f16a99
prabhak-rvs <prabhak@rvsgroup.com>
2026-09-21 13:34:54 +0530

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,30 +1,28 @@
-
-
 CREATE TABLE Department (
-DepartmentID INT PRIMARY KEY,
-DepartmentName VARCHAR(50) NOT NULL
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName VARCHAR(50)
 );
 
 CREATE TABLE Faculty (
-FacultyID INT PRIMARY KEY,
-FacultyName VARCHAR(50) NOT NULL,
-DepartmentID INT,
-FOREIGN KEY (DepartmentID)
-REFERENCES Department(DepartmentID)
+    FacultyID INT PRIMARY KEY,
+    FacultyName VARCHAR(50),
+    DepartmentID INT,
+    FOREIGN KEY (DepartmentID)
+        REFERENCES Department(DepartmentID)
 );
 
 CREATE TABLE Course (
-CourseID INT PRIMARY KEY,
-CourseName VARCHAR(50) NOT NULL,
-FacultyID INT,
-FOREIGN KEY (FacultyID)
-REFERENCES Faculty(FacultyID)
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(50),
+    FacultyID INT,
+    FOREIGN KEY (FacultyID)
+        REFERENCES Faculty(FacultyID)
 );
 
 CREATE TABLE Student (
-StudentID INT PRIMARY KEY,
-StudentName VARCHAR(50) NOT NULL,
-CourseID INT,
-FOREIGN KEY (CourseID)
-REFERENCES Course(CourseID)
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(50),
+    CourseID INT,
+    FOREIGN KEY (CourseID)
+        REFERENCES Course(CourseID)
 );
