0210b401d0609e6a110916ffc935d52f79474385
prabhak-rvs <prabhak@rvsgroup.com>
2026-09-25 11:01:18 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,5 +1,18 @@
-Create the following tables:
+CREATE DATABASE IF NOT EXISTS CollegeDB;
+USE CollegeDB;
 
--- Define appropriate Primary Keys and Foreign Keys.
+DROP TABLE IF EXISTS Student;
 
--- Write your SQL solution below
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(50),
+    CourseName VARCHAR(50),
+    FacultyName VARCHAR(50),
+    DepartmentName VARCHAR(50)
+);
+
+INSERT INTO Student VALUES
+(101, 'Arun', 'BCA', 'Dr. Kumar', 'Computer Science'),
+(102, 'Divya', 'BCA', 'Dr. Kumar', 'Computer Science'),
+(103, 'Karthik', 'BSc CS', 'Dr. Ravi', 'Computer Science'),
+(104, 'Meena', 'BBA', 'Dr. Priya', 'Management');
